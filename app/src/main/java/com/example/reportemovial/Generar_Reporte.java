f80464e90a92b9b9c4bc9b837607dcbc9a9ea3f7
David R <david.ramirez1503@hotmail.com>
2023-05-06 12:49:29 -0600

    implementacion de login

---
+++ app/src/main/java/com/example/reportemovial/Generar_Reporte.java
@@ -0,0 +1,17 @@
+package com.example.reportemovial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Generar_Reporte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generar_reporte);
+    }
+
+
+
+}
--- app/src/main/java/com/example/reportemovial/Login.java
+++ app/src/main/java/com/example/reportemovial/Login.java
@@ -2,14 +2,41 @@ package com.example.reportemovial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    private TextView txtUser;
+    private TextView txtPass;
+    private Button btnOlvidar;
+    private Button btnIniciar;
+    private Button btnRegistro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //login
-        System.out.println("Hola!");
+
+        txtUser = (TextView) findViewById(R.id.textUserLogin);
+        txtPass = (TextView) findViewById(R.id.textPassLogin);
+        btnOlvidar = (Button) findViewById(R.id.btnPassForgetLogin);
+        btnIniciar = (Button) findViewById(R.id.btnIniciarLogin);
+        btnRegistro = (Button) findViewById(R.id.btnRegistroLogin);
+
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), SignUp.class);
+                startActivity(i);
+            }
+        });
+
     }
+
+
+
 }
