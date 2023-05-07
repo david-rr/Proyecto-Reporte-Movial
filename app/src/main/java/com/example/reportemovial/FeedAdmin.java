e1a585e73857602d80cb78072d297f2b642517d7
Edme99 <edme999@gmail.com>
2023-05-07 12:39:42 -0600

    diseño de los feed

---
+++ app/src/main/java/com/example/reportemovial/FeedAdmin.java
@@ -0,0 +1,14 @@
+package com.example.reportemovial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FeedAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.feed_admin); //Feed de Ciudadano
+    }
+}
---
+++ app/src/main/java/com/example/reportemovial/FeedCiudadano.java
@@ -0,0 +1,14 @@
+package com.example.reportemovial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FeedCiudadano extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.feed_ciudadano); //Feed de Ciudadano
+    }
+}
