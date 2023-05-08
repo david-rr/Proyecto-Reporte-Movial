57c4d932bf50cc4a49876e883adf76db96529f1f
David R <david.ramirez1503@hotmail.com>
2023-05-08 10:51:14 -0600

    implementacion de login, falta mapview

--- app/src/main/java/com/example/reportemovial/Login.java
+++ app/src/main/java/com/example/reportemovial/Login.java
@@ -1,12 +1,23 @@
 package com.example.reportemovial;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
@@ -15,11 +26,13 @@ public class Login extends AppCompatActivity {
     private Button btnOlvidar;
     private Button btnIniciar;
     private Button btnRegistro;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //login
+        mAuth = FirebaseAuth.getInstance();
 
         txtUser = (TextView) findViewById(R.id.textUserLogin);
         txtPass = (TextView) findViewById(R.id.textPassLogin);
@@ -35,8 +48,56 @@ public class Login extends AppCompatActivity {
             }
         });
 
+        btnOlvidar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), Generar_Reporte.class);//crear ventana de recuperacion de contraseña
+                startActivity(i);
+            }
+        });
+
+        btnIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarSesion(txtUser.getText().toString(), txtPass.getText().toString());
+            }
+        });
+
+
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        }
+    }
 
+    private void iniciarSesion(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                            Intent i = new Intent(Login.this, Generar_Reporte.class);//crear ventana de recuperacion
+                            startActivity(i);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(Login.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            //updateUI(null); // reemplazar por nuevo activity
+                        }
+                    }
+                });
+    }
+
+    private void reload() {
+        Toast.makeText(Login.this, "Vuelve a intentarlo", Toast.LENGTH_SHORT).show();
+    }
 
 }
