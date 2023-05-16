5f5163b93147cd2625b123ff63c0a23b1fec7a93
David R <david.ramirez1503@hotmail.com>
2023-05-16 13:01:45 -0600

    feature generarReporte al 80% de funcionamiento. Falta corregir reporte con posibles elementos faltantes.

--- app/src/main/java/com/example/reportemovial/Generar_Reporte.java
+++ app/src/main/java/com/example/reportemovial/Generar_Reporte.java
@@ -4,15 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.location.Location;
-import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -22,7 +20,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -42,14 +39,23 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Generar_Reporte extends AppCompatActivity implements OnMapReadyCallback {
 
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private String tipo;
     private RadioButton tipo1;
+    private RadioButton tipo2;
+    private RadioButton tipo3;
+    private RadioButton tipo4;
     private EditText txtdesc;
     private Button btnObtUbi;
     private Button btnSelUbi;
@@ -58,12 +64,12 @@ public class Generar_Reporte extends AppCompatActivity implements OnMapReadyCall
     private Button btnEnviar;
     private ImageView foto;
     private LatLng point;
+    private FusedLocationProviderClient ubicacion;
 
     private GoogleMap nMap;
-    private FusedLocationProviderClient fusedLocationClient;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private StorageReference storageRef = FirebaseStorage.getInstance().getReference();
     private static final String TAG = "GenerarReporte";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -72,9 +78,6 @@ public class Generar_Reporte extends AppCompatActivity implements OnMapReadyCall
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.mapView);
         mapFragment.getMapAsync(this);
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
-
 
         txtdesc = (EditText) findViewById(R.id.editDescGenerarR);
         btnObtUbi = (Button) findViewById(R.id.btnObtenerUbGenerarR);
@@ -84,6 +87,30 @@ public class Generar_Reporte extends AppCompatActivity implements OnMapReadyCall
         btnEnviar = (Button) findViewById(R.id.btnEnviarReporteGenerarR);
         foto = (ImageView) findViewById(R.id.fotoGenerarR);
 
+        btnObtUbi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                obtenerUbicacion();
+            }
+        });
+
+        btnSelUbi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nMap.clear();
+                Toast.makeText(Generar_Reporte.this, "Manten presionado en el mapa para elegir ubicación", Toast.LENGTH_SHORT).show();
+                nMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+                    @Override
+                    public void onMapLongClick(@NonNull LatLng latLng) {
+                        nMap.clear();
+                        nMap.addMarker(new MarkerOptions().anchor(0.0f,1.0f).position(latLng));
+                        point = latLng;
+                    }
+                });
+            }
+        });
+
         btnCargarFoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -92,41 +119,27 @@ public class Generar_Reporte extends AppCompatActivity implements OnMapReadyCall
                 startActivityForResult(i.createChooser(i, "Seleccione la aplicacion"), 10);
             }
         });
-
-        btnObtUbi.setOnClickListener(new View.OnClickListener() {
+        
+        btnTomarFoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                nMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-                if (ActivityCompat.checkSelfPermission(Generar_Reporte.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(Generar_Reporte.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    txtdesc.setText("No se han definido los permisos necesarios");
-                    return;
-                }
-                fusedLocationClient.getLastLocation()
-                        .addOnSuccessListener(Generar_Reporte.this, new OnSuccessListener<Location>() {
-                            @Override
-                            public void onSuccess(Location location) {
-                                // Got last known location. In some rare situations this can be null.
-                                if (location != null) {
-                                    // Logic to handle location object
-                                    point = new LatLng(location.getLatitude(), location.getLongitude());
-                                    nMap.addMarker(new MarkerOptions().position(point).title("Ubicacion actual"));
-                                    CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(point, 17);
-                                    nMap.animateCamera(cameraUpdate);
-                                }
-                            }
-                        });
-
+                dispatchTakePictureIntent();
             }
         });
+
 
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                seleccionRB();
+                String img = enviarImagen();
+
                 Map<String, Object> user = new HashMap<>();
-                user.put("tipo", "Bache");
+                user.put("tipo", tipo);
                 user.put("descripcion", txtdesc.getText().toString());
-                user.put("imagen", "ruta/imagen2/prueba");
-                user.put("ubicacion", point.latitude);
+                user.put("imagen", img);
+                user.put("ubicacion", point);
+                user.put("estado", "Pendiente");
 
 
                 // Add a new document with a generated ID
@@ -136,26 +149,16 @@ public class Generar_Reporte extends AppCompatActivity implements OnMapReadyCall
                             @Override
                             public void onSuccess(DocumentReference documentReference) {
                                 Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                                Intent i = new Intent(v.getContext(), FeedCiudadano.class);
+                                startActivity(i);
+                                finish();
                             }
                         })
                         .addOnFailureListener(new OnFailureListener() {
                             @Override
                             public void onFailure(@NonNull Exception e) {
                                 Log.w(TAG, "Error adding document", e);
-                            }
-                        });
-                db.collection("users_adm")
-                        .get()
-                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                if (task.isSuccessful()) {
-                                    for (QueryDocumentSnapshot document : task.getResult()) {
-                                        String desc = document.get("descripcion").toString();
-                                    }
-                                } else {
-                                    System.out.println("Error al recuperar");
-                                }
+                                Toast.makeText(Generar_Reporte.this, "No se ha podido guardar el reporte.", Toast.LENGTH_SHORT).show();
                             }
                         });
             }
@@ -170,11 +173,110 @@ public class Generar_Reporte extends AppCompatActivity implements OnMapReadyCall
             Uri path = data.getData();
             foto.setImageURI(path);
         }
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            foto.setImageBitmap(imageBitmap);
+        }
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         nMap = googleMap;
+    }
+
+    public void seleccionRB(){
+        tipo1 = (RadioButton) findViewById(R.id.typeVialGR);
+        tipo2 = (RadioButton) findViewById(R.id.typeAguaGR);
+        tipo3 = (RadioButton) findViewById(R.id.typeAlumbradoGR);
+        tipo4 = (RadioButton) findViewById(R.id.typeAlumbradoGR);
+
+        if(tipo1.isChecked())
+            tipo = tipo1.getText().toString();
+        if(tipo2.isChecked())
+            tipo = tipo2.getText().toString();
+        if(tipo3.isChecked())
+            tipo = tipo3.getText().toString();
+        if(tipo4.isChecked())
+            tipo = tipo4.getText().toString();
+    }
+
+    private void obtenerUbicacion(){
+        if(ContextCompat.checkSelfPermission(Generar_Reporte.this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+            //Toast.makeText(this, "Tenemos permiso", Toast.LENGTH_SHORT).show();
+        } else {
+            ActivityCompat.requestPermissions(Generar_Reporte.this, new String[]{
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+
+        ubicacion = LocationServices.getFusedLocationProviderClient(this);
+        ubicacion.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null){
+                    point = new LatLng(location.getLatitude(), location.getLongitude());
+                    nMap.addMarker(new MarkerOptions().position(point).title("Ubicación actual"));
+                    nMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point, 15));
+                } else {
+                    Toast.makeText(Generar_Reporte.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private String cadenaAleatoria(int longitud) {
+        // El banco de caracteres
+        String banco = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        // La cadena en donde iremos agregando un carácter aleatorio
+        String cadena = "";
+        for (int x = 0; x < longitud; x++) {
+            int indiceAleatorio = numeroAleatorioEnRango(0, banco.length() - 1);
+            char caracterAleatorio = banco.charAt(indiceAleatorio);
+            cadena += caracterAleatorio;
+        }
+        return cadena+".jpg";
+    }
+
+    private int numeroAleatorioEnRango(int minimo, int maximo) {
+        // nextInt regresa en rango pero con límite superior exclusivo, por eso sumamos 1
+        return ThreadLocalRandom.current().nextInt(minimo, maximo + 1);
+    }
+
+    private String enviarImagen(){
+        String cadAux = cadenaAleatoria(40);
+        StorageReference archivoRef = storageRef.child("images/"+cadAux);
+
+        foto.setDrawingCacheEnabled(true);
+        foto.buildDrawingCache();
+        Bitmap bitmap = ((BitmapDrawable) foto.getDrawable()).getBitmap();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        UploadTask uploadTask = archivoRef.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Toast.makeText(Generar_Reporte.this, "No se ha podido guardar la imagen.", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                // ...
+            }
+        });
+
+        return cadAux;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 
 }
--- app/src/main/java/com/example/reportemovial/SignUp.java
+++ app/src/main/java/com/example/reportemovial/SignUp.java
@@ -12,6 +12,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -20,12 +21,15 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SignUp extends AppCompatActivity {
 
+    private EditText txtNom;
+    private EditText txtUser;
     private EditText txtCorreo;
-    private EditText txtCCorreo;
     private EditText txtPass;
-    private EditText txtCPass;
     private Button btnRegistrar;
     private Button btnIniciarSesion;
     private static final String TAG = "SignUp";
@@ -37,10 +41,10 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        txtCorreo = (EditText) findViewById(R.id.txtNomRegistro);
-        txtCCorreo = (EditText) findViewById(R.id.txtUserRegistro);
-        txtPass = (EditText) findViewById(R.id.txtCorreoRegistro);
-        txtCPass = (EditText) findViewById(R.id.txtContrasenaRegistro);
+        txtNom = (EditText) findViewById(R.id.txtNomRegistro);
+        txtUser = (EditText) findViewById(R.id.txtUserRegistro);
+        txtCorreo = (EditText) findViewById(R.id.txtCorreoRegistro);
+        txtPass = (EditText) findViewById(R.id.txtContrasenaRegistro);
         btnRegistrar = (Button) findViewById(R.id.btnRegistrarRegistro);
         btnIniciarSesion = (Button) findViewById(R.id.btnIniciarSesRegistro);
 
@@ -58,8 +62,8 @@ public class SignUp extends AppCompatActivity {
         btnRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if( txtCorreo.getText().toString().equals(txtCCorreo.getText().toString()) && txtPass.getText().toString()
-                        .equals(txtCPass.getText().toString())){ //reglas de registro
+                if( txtCorreo.getText().toString().equals(txtCorreo.getText().toString()) && txtPass.getText().toString()
+                        .equals(txtPass.getText().toString())){ //reglas de registro
                     registro(txtCorreo.getText().toString(), txtPass.getText().toString());
 
                 }
@@ -74,16 +78,29 @@ public class SignUp extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
+                            Map<String, Object> datos = new HashMap<>();
+                            datos.put("usuario", txtUser.getText().toString());
+                            datos.put("nombre", txtNom.getText().toString());
+                            datos.put("tipo", "ciudadano");
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
                             finish();
                             Toast.makeText(SignUp.this, "Usuario registrado correctamente",
                                     Toast.LENGTH_LONG).show();
+                            db.collection("usuarios_registrados")
+                                    .document(correo)
+                                    .set(datos)
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toast.makeText(SignUp.this, "No se ha podido registrar cuenta.", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(SignUp.this, "Authentication failed.",
+                            Toast.makeText(SignUp.this, "Error al crear cuenta",
                                     Toast.LENGTH_SHORT).show();
                         }
                     }
