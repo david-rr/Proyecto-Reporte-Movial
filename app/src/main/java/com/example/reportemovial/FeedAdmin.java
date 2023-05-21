fa25f8d4782768a6e0f9e3d3b283a72554f4fca8
Ganivet01 <iselacf1907@gmail.com>
2023-05-21 14:16:01 -0600

    Parte del menu desplegable

--- app/src/main/java/com/example/reportemovial/FeedAdmin.java
+++ app/src/main/java/com/example/reportemovial/FeedAdmin.java
@@ -1,16 +1,23 @@
 package com.example.reportemovial;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
@@ -26,6 +33,16 @@ public class FeedAdmin extends AppCompatActivity {
     public static PanelInfo Resumen = new PanelInfo();
 
     private Button btnVial, btnAgua, btnAlumbrado, btnArbol;
+
+    ///codigo para menu desplegable
+    DrawerLayout drawerLayout;
+    Button button;
+    //Button button2; //boton para el signo de sugerencias
+    LinearLayout VisReportes, Cerrarsesion;
+
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    //fin de codigo para menu
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,6 +107,53 @@ public class FeedAdmin extends AppCompatActivity {
             }
         });
 
+        //Boton que te dirige a la ayuda y sugerencias
+       // button2.setOnClickListener(new View.OnClickListener() {
+       //     @Override
+       //     public void onClick(View view) {
+       //         Intent i = new Intent(view.getContext(), Ayuda_y_sugerencias.class);//
+       //         startActivity(i);
+       //     }
+       // });
+        //Fin del boton que te lleva a ayuda y sugerencias
+
+
+        //Inicio Codigo para menu desplegable
+        drawerLayout = findViewById(R.id.drawer_layout2);
+        VisReportes = findViewById(R.id.VisReportes); //actividad Principal feed Admin
+        Cerrarsesion = findViewById(R.id.Cerrarsesion);
+        button = (Button) findViewById(R.id.button); //boton para desplegar el menu
+       // button2 = (Button) findViewById(R.id.button2); // botton para ir a las sugerencias
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openDrawer(drawerLayout);
+            }
+        });
+        VisReportes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recreate();
+            }
+        });
+
+
+        Cerrarsesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CerrarSession();
+            }
+        });
+
+
+
+        //fin Codigo para menu desplegable
+
     }
 
     @Override
@@ -120,4 +184,34 @@ public class FeedAdmin extends AppCompatActivity {
         ReportAdapter.stopListening();
     }
 
+    //codigo para menu desplegable
+    public static void openDrawer(DrawerLayout drawerLayout){
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+    public static void redirectActivity(Activity activity, Class secondActivity){
+        Intent intent = new Intent(activity, secondActivity);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+        activity.finish();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeDrawer(drawerLayout);
+    }
+    //fin de codigo para menu
+
+    //METODO PARA CERRAR SESION para el menu desplegable
+    private void CerrarSession(){
+        firebaseAuth.signOut();
+        Toast.makeText(this, "Ha cerrado sesión", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(FeedAdmin.this, Login.class));
+
+    }// Fin METODO PARA CERRAR SESION para el menu desplegable
 }
--- app/src/main/java/com/example/reportemovial/FeedCiudadano.java
+++ app/src/main/java/com/example/reportemovial/FeedCiudadano.java
@@ -1,10 +1,17 @@
 package com.example.reportemovial;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -16,6 +23,13 @@ public class FeedCiudadano extends AppCompatActivity {
     private ReporteAdapterCiudadano ReportAdapter;
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    ///codigo para menu desplegable
+    DrawerLayout drawerLayout;
+    Button button; //boton para accionar el menu desplegable
+    //Button button2; //boton para las sugerencias
+    LinearLayout VisReportes, MisReportes, CrearReportes, MiCuenta;
+    //fin de codigo para menu
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +47,61 @@ public class FeedCiudadano extends AppCompatActivity {
         ReportAdapter = new ReporteAdapterCiudadano(firestoreRecyclerOptions, FeedCiudadano.this);
         ReportAdapter.notifyDataSetChanged();
         RecyclerViewReporte.setAdapter(ReportAdapter);
+
+        //Inicio Codigo para menu desplegable
+        drawerLayout = findViewById(R.id.drawer_layout);
+        VisReportes = findViewById(R.id.VisReportes); //actividad Principal feed ciudadano
+        //MisReportes = findViewById(R.id.MisReportes);
+        CrearReportes = findViewById(R.id.CrearReportes);
+        //MiCuenta = findViewById(R.id.MiCuenta);
+        button = (Button) findViewById(R.id.button); //boton que activa el menu desplegable
+       // button2 = (Button) findViewById(R.id.button2); // botton para ir a las sugerencias
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openDrawer(drawerLayout);
+            }
+        });
+        VisReportes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                recreate();
+            }
+        });
+
+
+        CrearReportes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                redirectActivity(FeedCiudadano.this, Generar_Reporte.class);
+            }
+        });
+
+        //MisReportes.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View view) {
+        //        redirectActivity(FeedCiudadano.this, MisReportes.class);
+        //    }
+        //});
+
+        //MiCuenta.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View view) {
+        //        redirectActivity(FeedCiudadano.this, MisReportes.class);
+        //    }
+        //});
+
+       // button2.setOnClickListener(new View.OnClickListener() {
+       //     @Override
+       //     public void onClick(View view) {
+       //         Intent i = new Intent(view.getContext(), Ayuda_y_sugerencias.class);//
+       //         startActivity(i);
+       //     }
+       // });
+        //fin de codigo para menu desplegable
     }
 
     @Override
@@ -46,4 +115,28 @@ public class FeedCiudadano extends AppCompatActivity {
         super.onStop();
         ReportAdapter.stopListening();
     }
+
+    //codigo para menu desplegable
+    public static void openDrawer(DrawerLayout drawerLayout){
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+    public static void redirectActivity(Activity activity, Class secondActivity){
+        Intent intent = new Intent(activity, secondActivity);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+        activity.finish();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeDrawer(drawerLayout);
+    }
+    //fin de codigo para menu
+
 }
