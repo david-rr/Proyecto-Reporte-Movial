4f5eb02f210b8fa1e3467e0d36d8933217001f76
Ganivet01 <iselacf1907@gmail.com>
2023-05-21 18:33:35 -0600

    Nuevos cambios al menu desplegable

--- app/src/main/java/com/example/reportemovial/Alumbrado.java
+++ app/src/main/java/com/example/reportemovial/Alumbrado.java
@@ -1,15 +1,22 @@
 package com.example.reportemovial;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
@@ -19,6 +26,16 @@ public class Alumbrado extends AppCompatActivity {
     private ReporteAdapterFiltroTipo ReportAdapterAlumbrado;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private Button btnFeed, btnAgua, btnVial, btnArbol;
+
+    ///codigo para menu desplegable
+    DrawerLayout drawerLayout;
+    Button button;
+    Button button2; //boton para el signo de sugerencias
+    LinearLayout VisReportes, Cerrarsesion;
+
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    //fin de codigo para menu
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -74,6 +91,53 @@ public class Alumbrado extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        //Boton que te dirige a  sugerencias
+         button2 = (Button) findViewById(R.id.button2); // botton para ir a las sugerencias
+
+        button2.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent i = new Intent(view.getContext(), SugerenciaLayout.class);//
+                startActivity(i);
+             }
+         });
+        //Fin del boton que te lleva a ayuda y sugerencias
+
+
+        //Inicio Codigo para menu desplegable
+        drawerLayout = findViewById(R.id.drawer_layout4);
+        VisReportes = findViewById(R.id.VisReportes); //actividad Principal feed Admin
+        Cerrarsesion = findViewById(R.id.Cerrarsesion);
+        button = (Button) findViewById(R.id.button); //boton para desplegar el menu
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openDrawer(drawerLayout);
+            }
+        });
+        VisReportes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), FeedAdmin.class);//te lleva de la ventana actual al feed admin
+                startActivity(i);
+
+            }
+        });
+
+
+        Cerrarsesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                CerrarSession();
+            }
+        });//fin Codigo para menu desplegable
     }
 
     @Override
@@ -86,4 +150,36 @@ public class Alumbrado extends AppCompatActivity {
         super.onStop();
         ReportAdapterAlumbrado.stopListening();
     }
+
+    //codigo para menu desplegable
+    public static void openDrawer(DrawerLayout drawerLayout){
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+    public static void redirectActivity(Activity activity, Class secondActivity){
+        Intent intent = new Intent(activity, secondActivity);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+        activity.finish();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeDrawer(drawerLayout);
+    }
+    //fin de codigo para menu
+
+    //METODO PARA CERRAR SESION para el menu desplegable
+    private void CerrarSession(){
+        firebaseAuth.signOut();
+        Toast.makeText(this, "Ha cerrado sesión", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(Alumbrado.this, Login.class));
+
+    }// Fin METODO PARA CERRAR SESION para el menu desplegable
+
 }
